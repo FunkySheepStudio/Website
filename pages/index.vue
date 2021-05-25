f77f020d780fd3c782088321e5c8d21daf14561e
BartKnucle <sjarde@yahoo.fr>
2021-05-25 12:22:30 +0200

    Add content

--- pages/index.vue
+++ pages/index.vue
@@ -18,16 +18,17 @@
           justify="center"
         >
           <v-card
+            max-width="800"
             shaped
             color="rgb(0, 0, 0, 0.6)"
           >
             <v-card-title
               class="text-h4"
             >
-              {{ item.title }}
+              {{ item.title_en }}
             </v-card-title>
             <v-card-text>
-              {{ item.text }}
+              {{ item.en }}
             </v-card-text>
           </v-card>
         </v-row>
@@ -49,10 +50,10 @@
             color="rgb(0, 0, 0, 0.3)"
           >
             <v-card-title>
-              {{ item.title }}
+              {{ item.title_en }}
             </v-card-title>
             <v-card-text>
-              {{ item.text }}
+              {{ item.en }}
             </v-card-text>
             <v-img
               height="300"
@@ -71,36 +72,46 @@ export default {
     return {
       carousselItems: [
         {
-          title: 'Framework is Out!',
-          text: 'We are pleased to announce the first public version of our framework. It\'s time for us to release this tool to the community. Go check the documentation and examples to be able to download, use and contribute!',
+          title_en: 'Our framework is available!',
+          title_fr: 'Notre framework est disponible!',
+          en: 'We are delighted to announce the release of the alpha version of our framework. The community now has free access to a Unity-based framework to accelerate the network development of network projects. Do not hesitate to visit our examples page to discover the possibilities of the framework. All sources are available on our github page.',
+          fr: 'Nous sommes ravies de vous annoncer la sortie de la version alpha de notre framework. La communauté à désormais accés gratuitement à un framework basé sur Unity pour accélérer le développement réseau de projets réseaux. N\'hésitez pas à aller visiter notre page d\'examples pour découvrir les possibilité du framework. L\'ensemble des sources est disponible sur notre page github.',
           btnText: 'Discover',
           href: '/framework/description',
           src: 'https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?cs=srgb&dl=pexels-markus-spiske-2004161.jpg&fm=jpg'
         },
         {
-          title: 'Stay in touch',
-          text: 'Stay in touch with the team, github, discord etc...',
+          title_en: 'Keep in touch',
+          title_fr: 'Gardez le contact',
+          en: 'Contact our team on discord. Collaborate with us on Github. Watch our tutorials on youtube or send us your bitcoins to help us develop the framework ;) ...',
+          fr: 'Contactez notre équipe sur discord. Collaborez avec nous sur Github. Visionnez no tutos sur youtube ou envoyez nous vos bitcoins pour  nous aider au dévloppement du framwork ;)...',
           href: '/studio/contact',
           src: 'https://images.pexels.com/photos/3184424/pexels-photo-3184424.jpeg?cs=srgb&dl=pexels-fauxels-3184424.jpg&fm=jpg'
         }
       ],
       timeLineItems: [
         {
-          title: 'The studio',
+          title_en: 'The studio',
+          title_fr: 'Le studio',
           icon: 'mdi-account-multiple',
-          text: 'Funky Sheep Studio aim to provide a place for game developper to participate freely to inovative games. No leader, everyone evolved will be part of the entire projects.',
+          en: 'FunkySheep Studio is a company whose goal is to help companies implement graphics solutions for their customers. For each project we surround ourselves with specialists to best meet your needs.',
+          fr: 'FunkySheep Studio est une société dont le but est de d\'aider les entreprises à implémenter des solutions graphiques à destination de leurs clients. Pour chaque projet nous nous entourons de spécialistes pour satisfaire au mieux vos besoins.',
           image: 'https://images.pexels.com/photos/792051/pexels-photo-792051.jpeg?cs=srgb&dl=pexels-othmar-vigl-792051.jpg&fm=jpg'
         },
         {
-          title: 'The framework',
+          title_en: 'The framework',
+          title_fr: 'Le framework',
           icon: 'mdi-axis-arrow-info',
-          text: 'Build on NuxtJs, our open source framework allow anyone to integrate a powerfull tool to the unity developpement pipeline. It manage the network messaging, database integration and frontend quickly and efficiently.',
+          en: 'Built on NuxtJs, our open source framework allows everyone to integrate a powerful tool into their Unity 3D pipeline. It manages network messages, database integration and frontend quickly and efficiently.',
+          fr: 'Construit sur NuxtJs, notre framework open source permet à chacun d\'intégrer un outil puissant à son pipeline Unity 3D. Il gère les messages réseaux, l\'intégration à la base de donnée et le frontend rapidement et efficacement.',
           image: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?cs=srgb&dl=pexels-luis-gomes-546819.jpg&fm=jpg'
         },
         {
-          title: 'Our services',
+          title_en: 'Our services',
+          title_fr: 'Nos services',
           icon: 'mdi-cart',
-          text: 'Our time is yours. Everynone in our team is available to help you to integrate our framework to your pipeline, train you team or even create your game depending on your needs.',
+          en: 'Our time is yours. Our entire team is available to meet the challenges of your projects. We can help you integrate and use our framework, teach your teams how to use it or even create your games or content in real time. Our only limit is your imagination...',
+          fr: 'Notre temps est le votre. Toute notre équipe est disponible pour relever les challenge de vos projets. Nous pouvons vous aider à intégrer et utiliser notre framework, apprendre à vos équipes la prise en main de ce dernier ou même créer vos jeux ou contenus en temps réél. Notre seul limite est votre imagination...',
           image: 'https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?cs=srgb&dl=pexels-fauxels-3184465.jpg&fm=jpg'
         }
       ]
--- pages/studio/services.vue
+++ pages/studio/services.vue
@@ -1,8 +1,71 @@
 <template>
-  <section />
+  <v-container>
+    <p
+      class="text-center text-h3"
+    >
+      Our services
+    </p>
+    <v-timeline
+      :reverse="!$vuetify.breakpoint.mobile"
+      :dense="$vuetify.breakpoint.mobile"
+    >
+      <v-timeline-item
+        v-for="(item,i) in timeLineItems"
+        :key="i"
+        :icon="item.icon"
+      >
+        <v-card
+          shaped
+          elevation="10"
+          color="rgb(0, 0, 0, 0.3)"
+        >
+          <v-card-title>
+            {{ item.title_en }}
+          </v-card-title>
+          <v-card-text>
+            {{ item.en }}
+          </v-card-text>
+          <v-img
+            height="300"
+            :src="item.image"
+          />
+        </v-card>
+      </v-timeline-item>
+    </v-timeline>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      timeLineItems: [
+        {
+          title_en: 'Developpements',
+          title_fr: 'Developpements',
+          icon: 'mdi-code-tags',
+          en: 'The FunkySheep studio is able to help you in the development of your software. Whether in the field of real-time 3d, integration with your existing tools or even the development of a complete solution responding to your own flows.',
+          fr: 'Le studio FunkySheep est capacle de vous aider dans le développement de vos logiciels. Que ce soit dans le domaine de la 3d temps réélle, l\'intégration à vos outils existant ou même le developpement d\'une solution complête répondant à vos propres flux.',
+          image: 'https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        },
+        {
+          title_en: 'Integration',
+          title_fr: 'Integration',
+          icon: 'mdi-graph',
+          en: 'We support you in the integration and implementation of your work procedures thanks to DevOps techniques allowing a scaling up of your systems in real time. We go as far as the operation of your solutions, their maintenance and their upgrades as needed.',
+          fr: 'Nous vous accompagnons dans l\'integration et la mise en place de vos procédures de travail grace aux techniques DevOps permettant une montée en charge de vos systèmes en temps réél. Nous allons jusqu\'a l\'exploitation de vos solutions, leurs maintenance et leurs évolutions au besoin.',
+          image: 'https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?cs=srgb&dl=pexels-luis-gomes-546819.jpg&fm=jpg'
+        },
+        {
+          title_en: 'Training',
+          title_fr: 'Formation',
+          icon: 'mdi-school',
+          en: 'We can train your teams so that you are master of all the solutions that we put in place that you are completely autonomous on your entire system.',
+          fr: 'Nous pouvons former vos équipes pour que vous soyez maître de toutes les solutions que nous mettons en place que que vous soyez completement autonome sur l\'ensemble de votre système.',
+          image: 'https://images.pexels.com/photos/716276/pexels-photo-716276.jpeg?cs=srgb&dl=pexels-%E7%A5%9D-%E9%B9%A4%E6%A7%90-716276.jpg&fm=jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
